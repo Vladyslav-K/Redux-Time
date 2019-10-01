314447ddeaa63717fe81a6f383619d5a3d1dee2b
Vladyslav-K <your.reliable.man@gmail.com>
2019-10-01 18:34:23 +0300

    I learned action creators in Redux

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
-import { createStore } from 'redux'
-
-const reducer = (state = 0, action) => {
-
-  switch (action.type) {
-    case 'INC':
-      return state + 1
-    case 'DEC':
-      return state - 1
-    default:
-      return state
-  }
-}
+import { createStore, bindActionCreators } from 'redux'
+import reducer from './reducer'
+import * as actions from './actions'
 
 const store = createStore(reducer)
+const { dispatch, getState, subscribe } = store
+
+const { inc, dec, rnd } =
+  bindActionCreators(actions, dispatch)
 
 document
   .getElementById('inc')
-  .addEventListener('click', () => {
-    store.dispatch({ type: 'INC' })
-  })
+  .addEventListener('click', inc)
 
 document
   .getElementById('dec')
+  .addEventListener('click', dec)
+
+document
+  .getElementById('rnd')
   .addEventListener('click', () => {
-    store.dispatch({ type: 'DEC' })
+    const payload = Math.floor(Math.random() * 10)
+    rnd(payload)
   })
 
 const update = () => {
   document
     .getElementById('counter')
-    .innerHTML = store.getState()
+    .innerHTML = getState()
 }
 
-store.subscribe(update)
+subscribe(update)
