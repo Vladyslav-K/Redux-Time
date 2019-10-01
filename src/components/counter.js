1d48e6764babf9a826dc007df59652fec0b64a23
Vladyslav-K <your.reliable.man@gmail.com>
2019-10-01 21:39:22 +0300

    I learned the basics of Redux, time to move on!

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 const Counter = ({ counter, inc, dec, rnd }) => {
   return (
-    <div className="jumbotron">
+    <div className="container text-center">
 
-      <h2>{counter}</h2>
+      <div className="container">
+        <h2>{counter}</h2>
+      </div>
 
-      <button
-        onClick={dec}
-        className="btn btn-primary btn-large">
-        DEC
+      <div className="btn-group">
+        <button
+          onClick={dec}
+          className="btn btn-primary">
+          DEC
       </button>
 
-      <button
-        onClick={inc}
-        className="btn btn-primary btn-large">
-        INC
+        <button
+          onClick={inc}
+          className="btn btn-primary">
+          INC
       </button>
 
-      <button
-        onClick={rnd}
-        className="btn btn-primary btn-large">
-        RND
+        <button
+          onClick={rnd}
+          className="btn btn-primary">
+          RND
       </button>
-
-    </div>
+      </div>
+    </div >
   )
 }
 
@@ -35,4 +39,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(Counter)
+export default connect(mapStateToProps, actions)(Counter)
